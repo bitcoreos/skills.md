8390e0d84d0134f7bee132353ec4438dd9af2e8e
Bitcore Swarm <swarm@bitcore.io>
2026-01-17 03:51:11 +0000

    feat(skills): ingest anthropic customer-info and order-details

--- sources/anthropic/customer-info.js
+++ sources/anthropic/customer-info.js
@@ -5,21 +5,22 @@ origin: https://github.com/anthropics/anthropic-cookbook
 hash: sha256:ee8691aaa7e545e3c12ec627853c5d93cd47792b442371e34e255a6e157d1bc0
 version: 1.0.0
 signature: |
-  -----BEGIN PGP SIGNATURE-----
-  
-  iQIzBAABCgAdFiEEZ8U4LAM6MxGSR9xCicnEBsaHU18FAmlpmMQACgkQicnEBsaH
-  U1/DPw//XumCjT4WSC2V6QpbBTW5p8U2dsEegAtsJavBZuE/L6jRqCAsvkVLcZGd
-  Mo8njMU+Zid5X84royh6qoU5wK1nWXK246p+xNp9dO1/R0AHoklSjiHmpFL9YBKn
-  MwCcn8W6mL+lOdHuttEuaCgnPzPsEQW0rEKwIg0jh8cead/YeRhGJdIdQDhhugdL
-  2oLYasIXjd7Isl47hlkRbakP8Vs2UBsEZQyubDvitd4jSLCAppz8EL0SqG6l516P
-  DoSnZAFAoDFF98wep1/UTHmZsZXDvGwHCam70Ey1zuvCdvUIK/v3dxXq5NaogQJI
-  waiphPFBwrTIZ8GIKBurbLKXIuqAhjORFd8qRWI2dQcYJlj9UQ9ML0y/qntucFik
-  N0TsjErGNoWXlzJ4+pleELm4mu0xnW0uT8srzYDRbkavyT+vPKwZkFMTs2rKdfWy
-  4hcIOui6lwSrg6UTUWuXr0Ip4zXI9lYmd0ufx02VgvPu5cH/IEHyMPyO3WwBTQds
-  lB017sCdiofNY547Zg+kWGVf0YV1xucSX9YIFmMSku6bUgx+0HQSE7EFl3MQ5QSZ
-  a3cmHyDEc/crYtCcLdbXbDL+tZe67wFss1kq4ET7ZCD1gPZa4ncFczDlE+Lwrmlo
-  X+pjjUN670JPURMXygQ10Fv5IIGBRKZAn65HrQtJSc4EOMgLagA=
-  =3Ql1
-  -----END PGP SIGNATURE-----
+    -----BEGIN PGP SIGNATURE-----
+    
+    iQIzBAABCgAdFiEEZ8U4LAM6MxGSR9xCicnEBsaHU18FAmlrBy8ACgkQicnEBsaH
+    U1+8CA/+JYyMn9mynP9EIc+G6ycAMdo3RUXRfxSpTBDcqCvtbN+GV6Po14f4FOIC
+    GBXYiKVU1w4dfMRhmK3dGowTZEtH1RcXp5Ns3BHRVMXpJ7wnWEjdSNU/LDIJScdY
+    yQVD4CvfkLuZk5oeD3fYggMFDpOEd6DwCm5LGmeOcIv75RxTd9Ec3RC8b+ySXN/A
+    VvlqwIdry0mFvLJZKwcfJxViCAOYfJcZG3h08+EwvHK/3nCOAauV5Wvyqn4cNGsn
+    z4Ctfo5qrXmbgbzvBm1IBJk5nx765i/g3g0/X1k/p5WbMqIK2DU/vUg5tPtLiLlz
+    8bn0vVry6wjExa66bJrY7FvbaUFJRx1/jyV8orNkyxXP+wrkqynDckoaRWfThCAE
+    aCvofBaBUed6FTK5rqrjJcozGYst7wwsYmOMmNFcNETq91zzv8qANTdJGuhK9SdS
+    /SIwrxwB+2AHkpOQeWgogMmU2vV8yNVRLCBTzuU6YeZpVdy3pd6WjB4dGYlcf7ds
+    16zbWgW1gBtG/bSRGbxZn610X5qN8DN9fM2p2bwviEItzF00Z5dLRrunJeomuvqP
+    HI33rEQY2HiF+0y1vgMbZ0yHbggIJIB4X2E44Ed3KuvYRNERiEl9FIh4oDaz9WfY
+    CDgPyPJeB0w2XBcybY0tbKwNqVW++J8i68W3TWfoGSCZQntoT/w=
+    =dfIS
+    -----END PGP SIGNATURE-----
+
 ---
 export function getCustomerInfo(id) { const db = { "C1": {name: "John", email: "j@x.com"} }; return db[id] || "Not found"; }
--- sources/anthropic/order-details.js
+++ sources/anthropic/order-details.js
@@ -5,21 +5,22 @@ origin: https://github.com/anthropics/anthropic-cookbook
 hash: sha256:61060a9bc92d64a74bbf92608986f51179b20e3451762c522224479b14603332
 version: 1.0.0
 signature: |
-  -----BEGIN PGP SIGNATURE-----
-  
-  iQIzBAABCgAdFiEEZ8U4LAM6MxGSR9xCicnEBsaHU18FAmlpmMQACgkQicnEBsaH
-  U19tQg//Vi6ulcummScKVVOXDSxNSE7i7X6yCrz6DOzt7hZw9KG+jgUippmuZ0hS
-  LJY1BnXJcSCMzXK/MQ628swGaQf3MQJRN7vasH7lY/N8o6adztXE5x0YgywdgC8p
-  h/7gylgkWnSpMxovex0qjIYLVLeV4Z5I4u4hXztC3oKE0kaGjtCnD9VyaBcg+yhF
-  R52Ez8meY/tGD153zRWNSt8P86lqIQ9iwIaa0Iw5p9QNZrlQLQ2loRcrABUHHK+i
-  s0cbKHbJAmo31Ty5uJVs6f5vZkOeJcdOhe5ZF3LCYH8IiLjzTCt9teYjhimHcxHi
-  FMGBMIdMQTyNxm6V6rmjRKoOfOU3HGYSo8spbign7V8zFNC5e0t6Ma0umZ1mGjwW
-  jlJB+MwRS/POjmyHYHAdaiEDMgPWWV/9GIKIXEDtRzdhtliptGrmDdIxR45GXM/L
-  FiMLiDLL+WhUZn6S9+1JUhV+JTxWbn79sAE9G3R43T8AkiTo2Sc94ssM8lALte5e
-  1/9FGjN/Yc3cFrQ7LThdYOxPUj/g5QT3jEhRUUPYUHeNL6SmMo22Go86pE7+ZyoO
-  rBbNffvJrVOmSKqp4go+T+BJ5bimQbJhaq+yAIlT32S9vlPfT+H20FcjEWD1G4HS
-  cvpAy3/DaDz4MNnIQW5EZMcFydelg7zPj6C//OZr6BPp25kAfBA=
-  =zLHO
-  -----END PGP SIGNATURE-----
+    -----BEGIN PGP SIGNATURE-----
+    
+    iQIzBAABCgAdFiEEZ8U4LAM6MxGSR9xCicnEBsaHU18FAmlrBy8ACgkQicnEBsaH
+    U18xXxAAohXVKHDhsB3ko6ZEZJIIr2hwxo4CtvPHDxipgQYqtdDq1r7pdu/rae5d
+    pIZNVu7Hefcxhj3RHtxiqb0yGAE7+osFr5Yd2ROYKD4HupzvSZcygpNOQTW5CEPj
+    wwoRrncoAojNT5t/CJ0tUUOyf2f9l4KtKs8SUZCcchyDgP5u1TUHqCOv7Z6xubLO
+    Fkp/XjC6bXR+njsFv4Vpry3BdXzGyWzveFWuhFbOy8RqAPa/O1It5LJoTpjCNe1u
+    ai8OUPLzQpVMzd6VOydufHCKaNRyWXp1KaPaPH1PIJjVav//smNZZLtgVAJNqXUN
+    fByOW4WYZ2Rv0GRFP2RREv5gEEwm0pGgw/vCbXL7czIgrutv9sVpeofZsq3x0g3t
+    jxceVar+d+/EyeVXpDkXh7nvsf0nNiBIxeZtFKE11ueHVPmwpNZDVkI7e2wdk95D
+    2MTdDzDIQJe4r8E9wx8k++YFRrPjFw22x3AATgc0nVrfOCk4Z9bd1fEu5YVkcxni
+    0eZ21x4sZuUzO+4nsWiDbAs1VIpAhA3oiRoK4qPwtry+rBjN8M8vUqd1aFqQjIDZ
+    FH5XQ7Fyf9Ml21Pefu7dgB2x1jmviBcc5/F4qtMIiV6YqRLY2KuvDPUFm3QMYhwh
+    un5wipledmhtB5gmmfeTUQbX4rkELHyYz+6PPltCMn4RvzXJZ1Q=
+    =JhAe
+    -----END PGP SIGNATURE-----
+
 ---
 export function getOrderDetails(id) { const db = { "O1": {product: "Widget", price: 19.99} }; return db[id] || "Not found"; }
